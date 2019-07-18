8381c326ca422efd8b30aab18d5f8c3156f2e501
Will Brown <williambrown@williams-air.lan>
2019-07-17 23:46:13 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+var shoutouts = [
+  "You are really fun to talk to about concerts and you are very knowledgable.",
+  "You always make lunch really fun and I like talking to you about our debate struggles lmao",
+  "how you are able to create such creative things and persevere through any project.",
+  "I like your socks.",
+  "Your open mindedness.",
+  "You're very interesting to talk to and you seem like a really nice person!",
+  "your dry and subtle humor. Also, all the debate gossip I can talk about with you.",
+  "that passionate you are about what you enjoy (ie debate).",
+  "How interesting the questions you bring up during discussions are!",
+  "Your ability to grasp a concept so quickly and throughly.",
+  "How you have really creative ideas!",
+  "how helpful and kind you were during the sister sites interview process. I also really enjoy talking to you about our music similarities. You are so nice and kind and I am so glad that I got to know you:)",
+  "I like that you're always eager in getting to know everyone and looking for still being in touch  after the course is over.",
+  "I love you enthusiasm for everything we do and how determined you are."
+]
+
+for (x of shoutouts) {
+  $("#shoutout-container").append("<p>" + x + "</p> <br>");
+}
